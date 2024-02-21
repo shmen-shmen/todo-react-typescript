bff07cfb5f0f8aeb25c42ed3cf1384c71d48e827
shmen-shmen <akshevchenko@proton.me>
2024-02-22 00:46:00 +0600

    remove unused context file

--- src/context/actionNames.ts
+++ src/context/actionNames.ts
@@ -1,5 +1,4 @@
 export const ADD_TODO = "ADD_TODO";
 export const REMOVE_TODO = "REMOVE_TODO";
 export const EDIT_TODO = "EDIT_TODO";
-export const TODO_DONE = "TODO_DONE";
 export const TODO_REARRANGE = "TODO_REARRANGE";
